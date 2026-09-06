340a55e5b8f75ca65d4cbdf8ff1187dd0e7d5e4d
kons2003 <yeegilsu@gmail.com>
2026-09-07 00:28:08 +0900

    final 변수 추가

--- src/main/java/com/kons/dalnimtracker/dto/SightingResponseDto.java
+++ src/main/java/com/kons/dalnimtracker/dto/SightingResponseDto.java
@@ -7,15 +7,15 @@ import java.time.LocalDateTime;
 
 @Getter
 public class SightingResponseDto { // 서버 -> 사용자
-    private Integer id;
-    private Double latitude;
-    private Double longitude;
-    private String locationDescription;
-    private String content;
-    private String catStatus;
-    private String imageUrl;
-    private String reporterName;
-    private LocalDateTime createdAt;
+    private final Integer id;
+    private final Double latitude;
+    private final Double longitude;
+    private final String locationDescription;
+    private final String content;
+    private final String catStatus;
+    private final String imageUrl;
+    private final String reporterName;
+    private final LocalDateTime createdAt;
 
     // Entity를 받아서 안전한 DTO로 변환해 주는 생성자
     public SightingResponseDto(Sighting entity) {
